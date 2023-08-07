f786bcbc9cbd915e786593bdc4f193a14f1d9c15
hui lin <62810108+HuiLin0220@users.noreply.github.com>
2023-08-07 10:27:40 -0500

    Add files via upload

--- count_net_size.py
+++ count_net_size.py
@@ -1,6 +1,3 @@
-
-from ptflops import get_model_complexity_info
-
 import os
 import torch
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
@@ -8,17 +5,23 @@ from nnunetv2.utilities.plans_handling.plans_handler import PlansManager, Config
 from nnunetv2.utilities.get_network_from_plans1 import get_network_from_plans
 from batchgenerators.utilities.file_and_folder_operations import load_json
 
-#plan_path = "/home/hln0895/nnUNet/nnUNet_preprocessed/Dataset666_LA/nnUNetPlans.json"#"/home/hln0895/nnUNet/nnUNet_results/Dataset666_LA/nnUNetTrainer__nnUNetPlans__3d_fullres/fold_4_5M/result/plans.json"#"/home/hln0895/nnUNet/nnUNet_preprocessed/Dataset666_LA/nnUNetPlans.json"
-plan_path="/home/hln0895/nnUNet/nnUNet_results/Dataset777_LA/nnUNetTrainer__nnUNetPlans__3d_fullres/fold_0/result/plans.json"#"/home/hln0895/nnUNet/nnUNet_preprocessed/Dataset888_LA/nnUNetPlans.json"
+from ptflops import get_model_complexity_info
+
+
+plan_path = "/home/hln0895/Usformer/network_plan/Usformer_5M.json"
 plans = PlansManager(plan_path)
+
 configuration='3d_fullres'
 configuration_manager = plans.get_configuration(configuration)
 
-dataset_json_path="/home/hln0895/nnUNet/nnUNet_results/Dataset666_LA/nnUNetTrainer__nnUNetPlans__3d_fullres/dataset.json"
+dataset_json_path="/home/hln0895/Usformer/nnUNet_raw/Dataset666_LA/dataset.json"
 dataset_json = load_json(dataset_json_path)
-model=get_network_from_plans(plans, dataset_json, configuration_manager,
+
+model = get_network_from_plans(plans, dataset_json, configuration_manager,
                                       num_input_channels=1)
+
 macs, params = get_model_complexity_info(model, (1,32, 320,320), as_strings=True,
-                                           print_per_layer_stat=False, verbose=True)
+                                           print_per_layer_stat=False, verbose=True)#input size (1,32, 320,320),
+
 print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
 print('{:<30}  {:<8}'.format('Number of parameters: ', params))
