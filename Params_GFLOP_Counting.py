4cb4b3337bf5683ae940440a3ad56e756da35f92
hui lin <62810108+HuiLin0220@users.noreply.github.com>
2023-08-07 10:29:57 -0500

    Update Params_GFLOP_Counting.py

--- Params_GFLOP_Counting.py
+++ Params_GFLOP_Counting.py
@@ -8,16 +8,20 @@ from batchgenerators.utilities.file_and_folder_operations import load_json
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-plan_path = 'path_to_network_plan_file'
+plan_path = 'path_to_network_plan_file'#"/home/hln0895/Usformer/network_plan/Usformer_5M.json"
 plans = PlansManager(plan_path)
+
 configuration = '3d_fullres'
 configuration_manager = plans.get_configuration(configuration)
-dataset_json_path = 'path_to_dataset_json_file'
+
+dataset_json_path = 'path_to_dataset_json_file'#"/home/hln0895/Usformer/nnUNet_raw/Dataset666_LA/dataset.json"
 dataset_json = load_json(dataset_json_path)
 
 model = get_network_from_plans(plans, dataset_json, configuration_manager,
                                       num_input_channels=1)
+
 macs, params = get_model_complexity_info(model, (1,32, 320,320), as_strings=True,
                                            print_per_layer_stat=False, verbose=True)#input size(1,32, 320,320)
+
 print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
 print('{:<30}  {:<8}'.format('Number of parameters: ', params))
